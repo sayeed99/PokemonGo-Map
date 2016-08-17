696f9db02e8659b06c30f11a1126f39833dabd64
Sayeed Afridi (xda nickname : say99 ) <sayeed.afridi2009@gmail.com>
2016-08-17 21:35:25 +0530

    Example.py : Keep it more better.

--- example.py
+++ example.py
@@ -1,12 +1,13 @@
 # halpmepl0x
-# for those that messaged me with "i'm on dev, example.py dont work"
-# thunderfox : can we put an example.py in 2.0 that just prints "you're a fucking retard"
+# for those who messaged me with "i'm on dev, example.py doesn't works"
+# thunderfox : can we put an example.py in 2.0 that just prints "you're a fucking retard!"
 # invisiblek wuz here
 print("Please refer to documentation, there is no more example.py, only runserver.py")
-fuck = raw_input("Would yous till like to massage the devs that example.py no work?")
+print("This file is only used to check Python2.7 working. since raw_input() is only working in python 2.")
+fuck = raw_input("would you still like to message devs that example.py is not working?")
 if "needful" in fuck.lower():
     print("poon bang lure pls")
 elif fuck.lower() != "no":
-    print("you have been banned lol")
+    print("you have been banned lol!")
 else:
     print("bless u")
